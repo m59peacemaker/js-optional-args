8ed4b0f952e8da704aa62ced89dc0c7a4c4a2ade
Johnny Hauser <johnnyhauser@gmail.com>
2016-07-21 09:43:01 -0500

    v1.0.1 minor optimization

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const optionalArgs = (length, fn) => {
   return (...args) => {
     const fnLength = length || fn.length
     const missing = Math.max(0, fnLength - args.length)
-    args.splice(args.length - 1, 0, ...new Array(missing).fill(undefined))
+    missing && args.splice(args.length - 1, 0, ...new Array(missing))
     return fn(...args)
   }
 }
